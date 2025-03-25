7b94e695c20471167f16f340e348151f7594fa95
sojeongw <sojeongw.official@gmail.com>
2025-03-25 17:15:35 +0900

    init

---
+++ src/main/java/com/seejang/SeejangApplication.java
@@ -0,0 +1,13 @@
+package com.seejang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeejangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeejangApplication.class, args);
+    }
+
+}
