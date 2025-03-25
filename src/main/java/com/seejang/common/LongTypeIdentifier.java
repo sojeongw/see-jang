c4dd565d1c4d12b3e599dec4d8d83c24c205f9fb
sojeongw <sojeongw.official@gmail.com>
2025-03-25 17:46:17 +0900

    feat: 공통으로 사용할 클래스를 정의한다

---
+++ src/main/java/com/seejang/common/LongTypeIdentifier.java
@@ -0,0 +1,28 @@
+package com.seejang.common;
+
+
+public abstract class LongTypeIdentifier extends ValueObject<LongTypeIdentifier> {
+    private Long id;
+
+    public LongTypeIdentifier(Long id) {
+        this.id = id;
+    }
+
+    public Long longValue() {
+        return id;
+    }
+
+    public Long nextValue() {
+        return id + 1;
+    }
+
+    @Override
+    protected Object[] getEqualityFields() {
+        return new Object[] { id };
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + " : " + longValue();
+    }
+}
