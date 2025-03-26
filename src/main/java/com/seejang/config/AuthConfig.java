a5b641c6050f5d03e282663f9fea007e6c726477
sojeongw <sojeongw.official@gmail.com>
2025-03-26 15:09:02 +0900

    chore: 임시로 인증을 꺼둔다

---
+++ src/main/java/com/seejang/config/AuthConfig.java
@@ -0,0 +1,18 @@
+package com.seejang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AuthConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
